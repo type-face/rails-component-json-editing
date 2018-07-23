64a2aed9339e42d8ba7cb1623720063bef94a3ea
Rob Ring <rob@domain7.com>
2018-07-23 16:03:06 -0700

    added auth, chat, and messages

--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -9,6 +9,6 @@
 
 import "init";
 import "components/page/page";
+import "components/auth-form/auth-form";
+import "components/chat/chat";
 import "./application.css";
-
-document.body.insertAdjacentHTML("afterbegin", "Webpacker works!");
