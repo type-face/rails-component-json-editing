01f35741749169d18c8bb1caede867443233fa9c
Rob Ring <rob@domain7.com>
2018-07-25 14:27:03 -0700

    renamed app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,7 +54,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "modern-rails-frontend_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "soasher-prototype_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
 
